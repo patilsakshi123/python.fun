6befe94337e1538e1223338d58e83095e58b7024
patilsakshi123 <67007239+patilsakshi123@users.noreply.github.com>
2020-10-12 17:14:29 +0530

    Add files via upload

---
+++ fibonacci sequence.py
@@ -0,0 +1,19 @@
+nterms= int(input("how many terms:"))
+n1,n2= 0,1
+count=0
+if nterms <=0:
+    print("please enter a positive integer:")
+elif nterms ==1:
+    print("Fibonacci sequence upto", nterms ,":")
+else:
+    print("Fibonacci  sequence :")
+    while count < nterms :
+        print(n1)
+        nth =n1+n2
+        n1=n2
+        n2=nth
+        count+=1
+        
+
+            
+
